ccc680eaf349e529f7cfff03acb25d6e28485e7f
Umar Hashmi <hashmiumar642@gmail.com>
2024-07-30 21:30:42 +0500

    updates

--- amplify/index.d.ts
+++
@@ -1,6 +0,0 @@
-// amplify/index.d.ts
-declare module '@aws-amplify/backend' {
-    export const a: SpecificType;
-    export type ClientSchema<T> = T;
-}
-  
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,21 @@
-// src/index.tsx
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use the 'client' package for createRoot
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Amplify } from 'aws-amplify';
 
-// Get the current module's URL
 const moduleURL = new URL(import.meta.url);
-
-// Resolve the path of aws-exports.js relative to the module's URL
 const awsConfigPath = new URL('./aws-exports.js', moduleURL).pathname;
 
-// Import the actual JavaScript file
 import(awsConfigPath).then((awsconfig) => {
-    // Configure Amplify with the exported configuration
-    Amplify.configure(awsconfig.default);
+  console.log('AWS Config:', awsconfig.default); // Log the configuration
+  Amplify.configure(awsconfig.default);
 
-    // Create the root element and render the App
-    const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-    root.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    );
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}).catch((error) => {
+  console.error('Error loading AWS Config:', error); // Log any errors
 });
