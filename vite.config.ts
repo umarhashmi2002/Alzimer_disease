ac8add859a9ffe557de239ff4e095211ed27f072
Umar Hashmi <hashmiumar642@gmail.com>
2024-08-02 17:25:31 +0500

    npm build correction

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Provide alias for missing modules
-      '@aws-amplify/core/internals/utils': '@aws-amplify/core/dist/esm/internals/utils.js',
-    },
-  },
   build: {
     rollupOptions: {
+      // You can still keep this external option if needed
       external: ['@aws-amplify/core/internals/utils'],
     },
   },
